388a29abfbee17926c079f6ca525d45b134c55c0
Yusuke OSIRO <e185743@ie.u-ryukyu.ac.jp>
2019-11-07 12:02:53 +0900

    add sample/func

---
+++ Relational/Relational_repo/Relational/Relational/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Relational
+//
+//  Created by shinya yoshitaka on 2019/10/31.
+//  Copyright © 2019年 shinya yoshitaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
