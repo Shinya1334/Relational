69605f841d1a8c5cc8a1cd855029852425152925
Kaito Funatsuki <e185732@ie.u-ryukyu.ac.jp>
2019-11-21 10:30:02 +0900

    Add user model.

---
+++ Relational/Relational/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Relational
+//
+//  Created by Funatsuki Kaito on 2019/11/21.
+//  Copyright © 2019 shinya yoshitaka. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct User {
+    let name: String
+    let email: String
+    let passwords: String
+}
+
