4914970a5ddc5f331073a30acc55e6a4280ca446
shinya yoshitaka <ushimiyay20@gmail.com>
2019-11-21 10:25:21 +0900

    create home view

---
+++ Relational/Relational/MenuView.swift
@@ -0,0 +1,63 @@
+//
+//  MenuView.swift
+//  Relational
+//
+//  Created by shinya yoshitaka on 2019/11/21.
+//  Copyright © 2019 shinya yoshitaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack{
+                Image(systemName: "turtlerock")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("ホーム")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            HStack{
+                Image(systemName: "icybay")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("プロフィール")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            HStack{
+                Image(systemName: "lakemcdonld")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("最近使用した部屋")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+        HStack{
+            Image(systemName: "umbagog")
+                .foregroundColor(.gray)
+                .imageScale(.large)
+            Text("setting")
+                .foregroundColor(.gray)
+                .font(.headline)
+        }
+        .padding(.top, 30)
+        Spacer()
+    }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
