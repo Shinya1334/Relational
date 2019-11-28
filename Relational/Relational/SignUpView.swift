ea876eb706e82e406f629d58d959c27ff1822ebd
AzamA38 <e185731@ie.u-ryukyu.ac.jp>
2019-11-28 15:03:58 +0900

    change layout sign up view

--- Relational/Relational/SignUpView.swift
+++ Relational/Relational/SignUpView.swift
@@ -8,54 +8,97 @@
 
 import SwiftUI
 import Foundation
+import KeyboardObserving
 
 struct SignUpView: View {
-     // 1.
-       @State var maill = ""
-       @State var password = ""
-       @State var isShown = false
     
+    
+    
+//       @State var maill = ""
+//       @State var password = ""
+//       @State var isShown = false
+        @State var mailAdress = ""
+        @State var password = ""
+        @State var verifyPassword = ""
+//
        var body: some View {
-           VStack {
-            Text("メールアドレスを入力して下さい")
-                .font(.system(size: 25))
-            
-            TextField(" Enter some text", text: $maill)
-                .border(Color.black)
-            
-            Text("\(maill)")
-            
-            Text("パスワードを入力して下さい")
-            
-            TextField(" Enter some text", text: $password)
-                .border(Color.black)
-            
+        VStack {
+            KeyboardObservingView {
+            VStack {
+                Text("メールアドレス")
+                TextField("メールアドレスを入力してください", text: $mailAdress)
+                    .padding(.top, 30)
+                    .padding(.bottom, 40.0)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Text("パスワード")
+                SecureField("パスワードを入力してください", text: $password)
+                    .padding(.bottom, 40.0)
+                Text("パスワード(確認用)")
+                SecureField("上と同じパスワードを入力してください", text: $verifyPassword)
+                    .padding(.bottom, 40.0)
+            }
+            .padding()
+            }
+            Spacer()
+
             Button(action: {
-                self.isShown = true
+
             }) {
-                Text("Show ActionSheet")
+                Text("確認メールを送る")
             }
-            .actionSheet(isPresented: $isShown, content: {
-                            ActionSheet(
-                                title: Text("Title"),
-                                message: Text("Message"),
-                                buttons: [
-                                    .default(Text("Default")),
-                              .destructive(Text("Destructive")),
-                              .cancel()]
-                        )
-                    })
-                }
+            Spacer()
+            
             }
-           }
+        }
+    
+    
+//           VStack {
+//            Text("メールアドレスを入力して下さい")
+//                .font(.system(size: 25))
+//
+//            HStack {
+//                TextField(" Enter some text", text: $maill)
+//                    .border(Color.black)
+//                    .font(.system(size: 25))
+//                .padding()
+//            }
+//
+//            Text("パスワードを入力して下さい")
+//                .font(.system(size: 25))
+//
+//
+//            HStack {
+//                TextField(" Enter some text", text: $password)
+//                    .border(Color.black)
+//                    .font(.system(size: 25))
+//                .padding()
+//            }
+//
+//            Button(action: {
+//                self.isShown = true
+//            }) {
+//                Text("Sign Up")
+//                    .font(.system(size: 25))
+//            }
+////            .actionSheet(isPresented: $isShown, content: {
+////                            ActionSheet(
+////                                title: Text("サインしますか"),
+////                                buttons: [
+////                                    .default(Text("はい")),
+////                              .destructive(Text("いいえ")),
+////                              .cancel()]
+////                        )
+////                    })
+//        }
+//    }
+}
 
            //.padding()
            //.font(.title)
-
-
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
         SignUpView()
     }
 }
+
